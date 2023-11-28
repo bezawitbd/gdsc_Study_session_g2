62046264877eedd6b0be344d978247d14b558081
bezawitbd <bezawitdenbeze@gmail.com>
2023-11-28 11:13:12 -0800

    i commit the second commit

---
+++ project2.dart
@@ -0,0 +1,25 @@
+void main(){
+final numbers = [1, 8, 3,3,4,45,5,69,10,39,91,10];
+int max = numbers[0];
+int min = numbers[0];
+int sum = 0;
+double average 0.0;
+
+
+for (in t =0; i<numbers.length; i++);{
+    if (numbers[i] > max){
+        max = numbers[i];
+    }
+    if (numbers[i] < min){
+        min = numbers[i];
+    }
+    sum += numbers[i];
+}
+
+average = sum sum/ numbers.length;
+print('The maximum number is in the list is $max');
+print('The minimum number is in the list is $min');
+print('The sum of all number  in the list is $sum');
+print('The average of all number is in the list is $average');
+
+ }
